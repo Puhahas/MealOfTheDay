89c4800ae0a4cdd562f643dc108c1aea46ddebd3
lena <lenaminn@gmail.com>
2023-03-08 14:15:30 +0900

    Initial Commit

---
+++ motd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  motd
+//
+//  Created by Lena on 2023/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
